7b1be1898e371abf98186c54468208adc9787981
keerthi <keerthigongati2002@gmail.com>
2026-02-07 19:27:22 +0530

    adding first hava file

---
+++ source/add.java
@@ -0,0 +1,12 @@
+public class add {
+    public static int adding(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println("Result: " + adding(a, b));
+    }
+}
+
